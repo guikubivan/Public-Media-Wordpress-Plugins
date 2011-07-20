09e74d79c1953bc49a7042162843c77289833a8c
Priyank Shah <prishah@indiana.edu>
2011-07-20 12:05:08 -0400

    Added code for select menu for post image

--- wp-content/plugins/ipm-wordpress-slideshow-MVC/views/backend/admin_slideshow_wrapper.php
+++ wp-content/plugins/ipm-wordpress-slideshow-MVC/views/backend/admin_slideshow_wrapper.php
@@ -1,11 +1,26 @@
 
 <div style='padding:10px;' id='slideshow_options' >
 	<div style='float:right'>
-		<select style='display: none;' id='wpss_post_photo' class='".$this->plugin_prefix."required' name='wpss_post_photo'>
+		<select style='' id='wpss_post_photo' class='<?php echo $this->plugin_prefix."required" ?>' name='wpss_post_photo' onchange='setPostImage(this.value)'>
 			<option value=''>Choose post image</option>
+			<?php foreach($post_image_menu_titles as $id => $value)
+		{
+		if($id == $post_image_id){
+		?>
+		<option value="<?php echo $id ?>" selected="selected"><?php echo $value ?></option>
+		<?php
+		}
+		else{
+		?>
+		<option value="<?php echo $id ?>"><?php echo $value ?></option>
+		<?php
+		}
+		}
+		?>
 		</select>
 	</div>
 </div>
+</br>
 <ul id="slideshows_wrapper_ul">
 	<? foreach($slideshow_editors as $key => $slideshow_editor)
 	{
