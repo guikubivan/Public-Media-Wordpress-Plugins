ccae07cc8850cea53897d6988707eb2ba4505877
Priyank Shah <prishah@indiana.edu>
2011-08-26 11:33:56 -0400

    A new wrapper plugin for Breadcrumb-NavXT which helps display only the desired categories

---
+++ wp-content/plugins/breadcrumb-wrapper/wrapper.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Breadcrumb - Wrapper
+Plugin URI: http://www.wfiu.org
+Description: Wrapper plugin built for omitting the undesired catgories from the trail returned by the Breadcrumb NavXT plugin.
+Version: 1.0
+Author: Priyank Shah
+Author URI: http://www.wfiu.org
+*/
+
+
+function bcn_display_omit($omit=array()){
+	$string = bcn_display(true);
+	$temp = array();
+	$delimiter = "</span>";
+	$temp = explode($delimiter, $string);
+	$count = count($temp);
+		
+	foreach($omit as $remove){
+		for($i=1;$i<=$count;$i++){
+			if ((!strpos($temp[$i], $remove) == false) || ($temp[$i] == "")){
+				unset($temp[$i]);
+			}
+		}
+	}
+
+	$new_string ="";	
+
+	foreach($temp as $value){
+		$new_string.= $value."</span>";
+	}	
+	echo $new_string;
+}
+?>
