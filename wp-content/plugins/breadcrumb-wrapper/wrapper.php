4a543750d2ddc3d80ebf6570049273b602debb47
Priyank Shah <prishah@indiana.edu>
2011-08-26 11:59:13 -0400

    Added an if statement to check whether the Breadcrumb-navXT plugin exists

--- wp-content/plugins/breadcrumb-wrapper/wrapper.php
+++ wp-content/plugins/breadcrumb-wrapper/wrapper.php
@@ -10,25 +10,27 @@ Author URI: http://www.wfiu.org
 
 
 function bcn_display_omit($omit=array()){
-	$string = bcn_display(true);
-	$temp = array();
-	$delimiter = "</span>";
-	$temp = explode($delimiter, $string);
-	$count = count($temp);
+	if(function_exists('bcn_display')) {
+		$string = bcn_display(true);
+		$temp = array();
+		$delimiter = "</span>";
+		$temp = explode($delimiter, $string);
+		$count = count($temp);
 		
-	foreach($omit as $remove){
-		for($i=1;$i<=$count;$i++){
-			if ((!strpos($temp[$i], $remove) == false) || ($temp[$i] == "")){
-				unset($temp[$i]);
+		foreach($omit as $remove){
+			for($i=1;$i<=$count;$i++){
+				if ((!strpos($temp[$i], $remove) == false) || ($temp[$i] == "")){
+					unset($temp[$i]);
+				}
 			}
 		}
+
+		$new_string ="";	
+
+		foreach($temp as $value){
+			$new_string.= $value."</span>";
+		}	
+		echo $new_string;
 	}
-
-	$new_string ="";	
-
-	foreach($temp as $value){
-		$new_string.= $value."</span>";
-	}	
-	echo $new_string;
 }
 ?>
